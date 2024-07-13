20b34186c1c855d35ccf7ddc0b7f91b2d1d06929
esteban <estebangagneur03@gmail.com>
2024-07-13 12:32:41 +0200

    tree farming block identifier

--- tree-farming.lua
+++ tree-farming.lua
@@ -1,9 +1,9 @@
-while true do
-    sleep(1)
-    plant()
-end
-
 function plant()
     local hasBlock, data = turtle.inspect()
     print(hasBlock, textutils.serialize(data))
+end
+
+while true do
+    sleep(1)
+    plant()
 end
